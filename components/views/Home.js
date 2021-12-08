2752898c5561fce05b8b7c8a6beced806e5cc93c
Jeff <jeffrey.edson@pm.me>
2021-12-08 05:27:11 -0600

    begin conversion to SPA

---
+++ components/views/Home.js
@@ -0,0 +1,14 @@
+export default () => html`
+  <section id="jumbotron">
+    <h2>Savvy Coders Javascript Fullstack Cohort</h2>
+    <a href="" onClick="alert('Hello! You clicked the Button!')"
+      >"Call to Action Button"</a
+    >
+  </section>
+  <section id="shark">
+    <img src="${sharkImage}" />
+  </section>
+
+
+  
+`;
