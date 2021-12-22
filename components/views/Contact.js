634eafcfaf8865ff1ba3a97dd739652ebb3bc9eb
Jeff <jeffrey.edson@pm.me>
2021-12-22 07:30:25 -0600

    fix main container height, add formspree form

--- components/views/Contact.js
+++ components/views/Contact.js
@@ -1,28 +1,19 @@
 import html from "html-literal";
 
 export default () => html`
-  <form action="" method="POST">
-    <label for="name">Name:</label>
-    <input type="text" name="name" id="name" placeholder="Full Name" /><br />
+<form id="fs-frm" name="simple-contact-form" accept-charset="utf-8" action="https://formspree.io/f/xqknkrqw" method="post">
+  <fieldset id="fs-frm-inputs">
+    <label for="full-name">Full Name</label>
+    <input type="text" name="name" id="full-name" placeholder="First and Last" required="">
 
-    <label for="email">Email:</label>
-    <input
-      type="email"
-      name="email"
-      id="email"
-      placeholder="example@gmail.com"
-    /><br />
-    <div>
-      <label for="msg"></label>
-      <textarea
-        name="msg"
-        id="msg"
-        placeholder="Please, enter your message here."
-        cols="50"
-        rows="10"
-      ></textarea>
-    </div>
+    <label for="email-address">Email Address</label>
+    <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required="">
 
-    <input type="submit" value="Submit" />
-  </form>
+    <label for="message">Message</label>
+    <textarea rows="5" name="message" id="message" placeholder="Message" required=""></textarea>
+    
+    <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission">
+  </fieldset>
+  <input type="submit" value="Submit">
+</form>
 `;
