8742af4a0483ae6c4433dc23158df9f6b457475f
Jeff <jeffrey.edson@pm.me>
2022-01-06 21:55:25 -0600

    Remove html from template literal in gardens view

--- components/views/Gardens.js
+++ components/views/Gardens.js
@@ -1,11 +1,9 @@
-import html from "html-literal";
-
-export default st => html`
+export default st => `
 <table id="gardens">
 <tr><th>Garden Name</th><th>City</th></tr>
 ${st.gardens
   .map(garden => {
-    return `<tr><td>${garden.nameOfGarden}</td><td>${garden.location.city}</td>`;
+    return `<tr><td>${garden.nameOfGarden}</td><td>${garden.location.city}</td></tr>`;
   })
   .join("")}
 </table>
