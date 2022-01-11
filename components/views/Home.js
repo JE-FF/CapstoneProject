6f1df6afb40122cd6fbd80f68368f0cc52e391be
Jeff <jeffrey.edson@pm.me>
2022-01-11 08:39:17 -0600

    Add navigation container to home page.

--- components/views/Home.js
+++ components/views/Home.js
@@ -3,4 +3,20 @@ import gardenBackground from "/assets/img/jed-owen-1JgUGDdcWnM-unsplash.jpg";
 
 export default (st) => html`
   <img src="${gardenBackground}" />
+  <div id="homeNavContainer">
+    <ul>
+      <li>Grow</li>
+      <li>Eat</li>
+      <li>Learn</li>
+    </ul>
+    <div id="growPane">
+      GROW PANE TEXT HERE
+    </div>
+    <div id="eatPane">
+      EAT PANE TEXT HERE
+    </div>
+    <div id="learnPane">
+      LEARN PANE TEXT HERE
+    </div>
+  </div>
 `;
