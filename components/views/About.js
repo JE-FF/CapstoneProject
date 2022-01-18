b13e6abefa784c75a55f8071b4972e7bd6e57ff1
Jeff <jeffrey.edson@pm.me>
2022-01-18 10:01:48 -0600

    About page refactor, style fixes, footer update.

--- components/views/About.js
+++ components/views/About.js
@@ -1,12 +1,16 @@
 import html from "html-literal";
 
 export default () => html`
-  <section id="bio">
-    <p>Grown @ Home is a platform for gardeners to sell their home-grown produce
+  <div class="pageDescription">
+    <h1>About Us</h1>
+    <br />
+    <p>
+      Grown @ Home is a platform for gardeners to sell their home-grown
+      produce,<br />
       and for consumers to purchase fresh vegetables from their neighbors.
-      <br>
-      <br>
-      Founded in 2022. 
+      <br /><br />
+      Founded in Independence, Missouri 2022.
     </p>
-  </section>
+    <br />
+  </div>
 `;
